4910fe743e9003c28cf95b767e37259270e6cb5c
k00dy4172 <k00d24172@gmail.com>
2026-06-30 11:11:19 +0900

    Initial project setup

---
+++ src/hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "hello.h"
+
+void PrintHello()
+{
+    std::cout << "Hello from another file!\n";
+}
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+﻿// main.cpp : 애플리케이션의 진입점을 정의합니다.
+//
+
+#include "main.h"
+#include "hello.h"
+
+using namespace std;
+
+int main()
+{
+	PrintHello();
+	return 0;
+}
